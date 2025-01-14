1209538d255b8a5ded1a6e4a56263c38fc88b90f
Michael  Wheeler <mwheeler2244@yahoo.com>
2025-01-14 10:13:40 -0600

    types

--- app/[search]/page.tsx
+++ app/[search]/page.tsx
@@ -5,24 +5,29 @@ type Props = {
     search: string;
   };
 };
+export const dynamic = "force-dynamic";
 
 export default async function SearchWiki({ params: { search } }: Props) {
-  const wikiData: Promise<SearchResult> = await WikiApi(search);
-  const data = await wikiData;
-  const results: Result[] | undefined = data?.query?.pages;
+  try {
+    const wikiData: Promise<SearchResult> = await WikiApi(search);
+    const data = await wikiData;
+    const results: Result[] | undefined = data?.query?.pages;
 
-  const answer = results ? (
-    Object.values(results).map((result) => (
-      <div
-        key={result.pageid}
-        className="p-4 border-2 border-gray-300 rounded-lg shadow-md mb-4 hover:bg-gray-100"
-      >
-        <Item result={result} />
-      </div>
-    ))
-  ) : (
-    <p className="text-center text-lg text-red-500">{`${search} not found`}</p>
-  );
+    const answer = results ? (
+      Object.values(results).map((result) => (
+        <div
+          key={result.pageid}
+          className="p-4 border-2 border-gray-300 rounded-lg shadow-md mb-4 hover:bg-gray-100"
+        >
+          <Item result={result} />
+        </div>
+      ))
+    ) : (
+      <p className="text-center text-lg text-red-500">{`${search} not found`}</p>
+    );
 
-  return <div className="m-14">{answer}</div>;
+    return <div className="m-14">{answer}</div>;
+  } catch (error) {
+    console.error(error);
+  }
 }
